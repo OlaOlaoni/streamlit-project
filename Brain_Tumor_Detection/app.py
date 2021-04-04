3b4c2280d7233666fc0cd414027e93183ea93baa
Olanrewaju Olaonipekun <OOlaonipekun@gcn.ab-inbev.com>
2021-04-04 18:53:57 +0100

    handled typeerror

--- Brain_Tumor_Detection/app.py
+++ Brain_Tumor_Detection/app.py
@@ -7,7 +7,6 @@ st.header("Brain Tumor MRI Classification Example")
 st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
 
 uploaded_file = st.file_uploader("Choose a brain MRI ...", type=['jpeg','jpg','png'])
-st.write("")
 
 if uploaded_file is not None:
         image = Image.open(uploaded_file)
@@ -17,5 +16,7 @@ if uploaded_file is not None:
         label = teachable_machine_classification(image, 'keras_model.h5')
         if label == 0:
             st.write("The MRI scan has a brain tumor")
-        else:
+        elif label == 1:
             st.write("The MRI scan is healthy")
+        else:
+            raise TypeError()
